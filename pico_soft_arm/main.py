6a3f4a9b228e80c074062c7558351ff49fff922f
Ivan Iziumov <i.izyumov@marketlab.pl>
2022-08-07 23:36:17 +0200

    Modules modifications

--- pico_soft_arm/main.py
+++ pico_soft_arm/main.py
@@ -7,17 +7,10 @@ import time
 analog_pin = 28
 analog_channels_start = 2
 number_of_analog_channels = 10
-pwm_pin = 11
-dirr_pin = 10
-last_value = 0
-
-pwm = PWM(Pin(pwm_pin))
-pwm.freq(10000)
 
 with open('config.json', 'r') as conf:
     config = json.load(conf)
 
-dirr = Pin(dirr_pin, Pin.OUT)
 pot_read = ADC(analog_pin)
 pot_select_pins = list([Pin(pin_number, Pin.OUT) for pin_number in range(analog_channels_start, analog_channels_start + number_of_analog_channels)])
 motors = list([Motor(key["min"], key["max"], key["pwm_min"], key["dirr_pin"]) for key in config])
@@ -37,11 +30,8 @@ while True:
     for current_channel in range(number_of_analog_channels):
         for index, current_pot in enumerate(pot_select_pins):
             current_pot.value(current_channel >> index & 1)
-        current_pot_value = map(pot_read.read_u16(), 0, 65355, 0, 1)
-        dataset.append('{:.3f}'.format(current_pot_value))
-        
-    last_value += pid.regulate(eval(dataset[0]), last_value, 0.01)
+        current_pot_value = pot_read.read_u16()
+        dataset.append(str(current_pot_value))
     
-    combined = '{}\n'.format('|'.join(dataset))
-    uart.write(combined)
+    uart.write('{}\n'.format('|'.join(dataset)))
     
--- pico_soft_arm/motor.py
+++ pico_soft_arm/motor.py
@@ -1,18 +1,19 @@
 from machine import PWM, Pin
 
 class Motor:
-    def __init__(self, minimal, maximal, pwm_pin, dirr_pin, initial_dir = True, pwm_freq = 10000):
+    def __init__(self, minimal, maximal, pwm_pin, dirr_pin, initial_dirr = True, pwm_freq = 10000):
         self.minimal = minimal
         self.maximal = maximal
         self.pwm_pin = PWM(Pin(pwm_pin))
         self.pwm_pin.freq(pwm_freq)
+        self.initial_dirr = initial_dirr
+        self.current_dirr = initial_dirr
         self.dirr_pin = Pin(dirr_pin, Pin.OUT)
-        self.initial_dir = initial_dir
-        self.dirr_pin.value(self.initial_dir)
+        self.dirr_pin.value(self.current_dirr)
         
     def change_dir(self):
-        self.initial_dir != self.initial_dir
-        self.dirr_pin.value(self.initial_dir)
+        self.current_dirr != self.current_dirr
+        self.dirr_pin.value(self.current_dirr)
 
     def set_duty(self, duty):
         self.pwm_pin.duty_u16(duty)
