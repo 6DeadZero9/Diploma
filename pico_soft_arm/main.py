c162f14635f2d2fb6a40630665d0aa3a20931bf5
Ivan Iziumov <iv.izyumov@gmail.com>
2022-09-07 00:51:27 +0200

    Modified prosthetic software

--- pico_soft_arm/main.py
+++ pico_soft_arm/main.py
@@ -1,23 +1,29 @@
 from machine import UART, ADC, PWM, Pin
 from motor import Motor
-from PID import PID
 import json
 import time
 
 analog_pin = 28
+adc = ADC(27)
 analog_channels_start = 2
 number_of_analog_channels = 10
+
+min_speed = 20000
+max_speed = 65535
 
 with open('config.json', 'r') as conf:
     config = json.load(conf)
 
 pot_read = ADC(analog_pin)
-pot_select_pins = list([Pin(pin_number, Pin.OUT) for pin_number in range(analog_channels_start, analog_channels_start + number_of_analog_channels)])
-motors = list([Motor(key["min"], key["max"], key["pwm_min"], key["dirr_pin"]) for key in config])
-pid = PID(0.1, 1, 0, 0, 1)
-uart = UART(0, 115200)
+pot_select_pins = list([Pin(pin_number, Pin.OUT) for pin_number in range(analog_channels_start, analog_channels_start + 4)])
+motors = {key: Motor(config[key]["min"],
+                     config[key]["max"],
+                     config[key]["IA1"],
+                     config[key]["IA2"],
+                     config[key]["pot_index"],
+                     config[key]["emg_index"],) for key in config}
 
-def map(value, leftMin, leftMax, rightMin, rightMax):
+def map_values(value, leftMin, leftMax, rightMin, rightMax):
     leftSpan = leftMax - leftMin
     rightSpan = rightMax - rightMin
 
@@ -31,7 +37,30 @@ while True:
         for index, current_pot in enumerate(pot_select_pins):
             current_pot.value(current_channel >> index & 1)
         current_pot_value = pot_read.read_u16()
-        dataset.append(str(current_pot_value))
-    
-    uart.write('{}\n'.format('|'.join(dataset)))
+        dataset.append(current_pot_value)
+        
+    reading = adc.read_u16()
+     
+    for current_motor in motors:
+        motors[current_motor].set_current_value(dataset[motors[current_motor].pot_index])
+        #reading = dataset[motors[current_motor].emg_index]
+        
+        if motors[current_motor].emg_min is None or reading < motors[current_motor].emg_min:
+            motors[current_motor].emg_min = reading
+        if motors[current_motor].emg_max is None or reading > motors[current_motor].emg_max:
+            motors[current_motor].emg_max = reading
+            
+        try:
+            goal_mapped = int(map_values(reading, motors[current_motor].emg_min, motors[current_motor].emg_max,
+                                         motors[current_motor].minimum, motors[current_motor].maximum))
+            diff = goal_mapped - motors[current_motor].current_value
+            diff_sign = diff >= 0
+            diff = abs(diff)
+            diff_mapped = int(map_values(motors[current_motor].minimum + diff, motors[current_motor].minimum,
+                                         motors[current_motor].maximum, min_speed, max_speed))
+            motors[current_motor].set_duty(diff_mapped, diff_sign)
+        except Exception as error:
+            pass
+
+    time.sleep(0.1)
     
--- pico_soft_arm/motor.py
+++ pico_soft_arm/motor.py
@@ -1,19 +1,30 @@
 from machine import PWM, Pin
 
 class Motor:
-    def __init__(self, minimal, maximal, pwm_pin, dirr_pin, initial_dirr = True, pwm_freq = 10000):
-        self.minimal = minimal
-        self.maximal = maximal
-        self.pwm_pin = PWM(Pin(pwm_pin))
-        self.pwm_pin.freq(pwm_freq)
-        self.initial_dirr = initial_dirr
-        self.current_dirr = initial_dirr
-        self.dirr_pin = Pin(dirr_pin, Pin.OUT)
-        self.dirr_pin.value(self.current_dirr)
-        
-    def change_dir(self):
-        self.current_dirr != self.current_dirr
-        self.dirr_pin.value(self.current_dirr)
+    def __init__(self, minimum, maximum, IA1, IA2, pot_index, emg_index, pwm_freq = 10000):
+        self.pot_index = pot_index
+        self.emg_index = emg_index
+        self.minimum = minimum
+        self.maximum = maximum
+        self.emg_min = None
+        self.emg_max = None
+        self.IA1 = PWM(Pin(IA1))
+        self.IA2 = PWM(Pin(IA2))
+        self.IA1.freq(pwm_freq)
+        self.IA2.freq(pwm_freq)
+        self.current_value = 0
+    
+    def set_current_value(self, value):
+        if self.minimum <= value <= self.maximum:
+            self.current_value = value
+        else:
+            self.current_value = self.minimum if value <= self.minimum else self.maximum
 
-    def set_duty(self, duty):
-        self.pwm_pin.duty_u16(duty)
+    def set_duty(self, duty, dirr = True):
+        if dirr:
+            self.IA1.duty_u16(duty)
+            self.IA2.duty_u16(0)
+        else:
+            self.IA1.duty_u16(0)
+            self.IA2.duty_u16(duty)
+
